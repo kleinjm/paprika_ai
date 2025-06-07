ccaefa2b7e9e783b93080b0f305f8ef7fa9f21e2
James Klein <kleinjm007@gmail.com>
2025-06-07 12:07:12 -0600

    fix grocery relationships

--- app/models/paprika/grocery_item.rb
+++ app/models/paprika/grocery_item.rb
@@ -29,7 +29,32 @@
 module Paprika
   class GroceryItem < ApplicationRecord
     self.table_name = "ZGROCERYITEM"
-    belongs_to :grocery_list, class_name: "Paprika::GroceryList", foreign_key: "ZGROCERYLIST"
-    belongs_to :grocery_aisle, class_name: "Paprika::GroceryAisle", foreign_key: "ZGROCERYAISLE", optional: true
+
+    attribute :name, :string
+    alias_attribute :name, :ZNAME
+    attribute :quantity, :string
+    alias_attribute :quantity, :ZQUANTITY
+    alias_attribute :purchased, :ZPURCHASED
+    alias_attribute :separated, :ZSEPARATE
+    alias_attribute :aisle, :ZAISLE
+    alias_attribute :aisle_name, :ZAISLENAME
+    alias_attribute :instruction, :ZINSTRUCTION
+    alias_attribute :recipe_name, :ZRECIPENAME
+    alias_attribute :status, :ZSTATUS
+    alias_attribute :synced, :ZISSYNCED
+    alias_attribute :order_flag, :ZORDERFLAG
+    alias_attribute :uid, :ZUID
+    alias_attribute :ingredient, :ZINGREDIENT
+
+    belongs_to :grocery_list,
+      class_name: "Paprika::GroceryList",
+      foreign_key: "ZLIST"
+    belongs_to :grocery_aisle,
+      class_name: "Paprika::GroceryAisle",
+      foreign_key: "ZAISLE",
+      optional: true
+
+    scope :purchased, -> { where(ZPURCHASED: 1) }
+    scope :unpurchased, -> { where(ZPURCHASED: 0) }
   end
 end
--- app/models/paprika/grocery_list.rb
+++ app/models/paprika/grocery_list.rb
@@ -20,6 +20,8 @@
 module Paprika
   class GroceryList < ApplicationRecord
     self.table_name = "ZGROCERYLIST"
-    has_many :grocery_items, class_name: "Paprika::GroceryItem", foreign_key: "ZGROCERYLIST"
+    has_many :grocery_items,
+      class_name: "Paprika::GroceryItem",
+      foreign_key: "ZLIST"
   end
 end
